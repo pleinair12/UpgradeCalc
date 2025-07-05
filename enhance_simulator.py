36a6619070fbc6f227a77d6222340aeed2f5bbac
pleinair12 <70314961+pleinair12@users.noreply.github.com>
2025-07-06 01:43:09 +0900

    Update enhance_simulator.py

--- enhance_simulator.py
+++ enhance_simulator.py
@@ -71,8 +71,8 @@ def simulate_enhance(start_level, end_level):
 
 # Streamlit UI
 st.sidebar.header("시뮬레이션 설정")
-start_level = st.sidebar.number_input("시작 강화 수치", min_value=0, max_value=29, value=10)
-default_end = max(start_level + 1, 20)
+start_level = st.sidebar.number_input("시작 강화 수치", min_value=0, max_value=29, value=0)
+default_end = max(start_level + 1, 25)
 end_level = st.sidebar.number_input("목표 강화 수치", min_value=start_level + 1, max_value=30, value=default_end)
 simulations = st.sidebar.number_input("시뮬레이션 반복 횟수", min_value=1, max_value=10000, value=1000)
 
